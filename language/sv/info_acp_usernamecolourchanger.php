5141e55de111b8f159a103b7e93dbf08e11c5e4a
dmzx <info@dmzx-web.net>
2015-08-31 19:39:58 +0200

    Swedish translation

---
+++ language/sv/info_acp_usernamecolourchanger.php
@@ -0,0 +1,36 @@
+<?php
+/**
+* Username Colour Changer [Swedish]
+* Swedish translation by Kimmy (http://www.dreadheads.se)
+*
+* @package phpBB Extension - Username Colour Changer
+* @copyright (c) 2015 dmzx - http://www.dmzx-web.net
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+
+$lang = array_merge($lang, array(
+	'ACL_U_USERNAMECOLOURCHANGER_USE'	=> 'Kan ändra färg på sitt användarnamn',
+));
